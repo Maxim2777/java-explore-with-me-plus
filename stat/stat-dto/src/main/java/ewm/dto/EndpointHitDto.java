deeb77e54cc282471cc06e8897120384619f96d1
2777maxim <2777maxim@mail.ru>
2025-03-19 16:20:12 +0300

    Исправленный DTO

--- stat/stat-dto/src/main/java/ewm/dto/EndpointHitDto.java
+++ stat/stat-dto/src/main/java/ewm/dto/EndpointHitDto.java
@@ -1,17 +1,29 @@
 package ewm.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EndpointHitDto {
+    @NotNull
     private String app;
+
+    @NotEmpty
+    @Pattern(regexp = "^/.*") // Убедиться, что `uri` начинается с `/`
     private String uri;
+
+    @NotEmpty
+    @Pattern(regexp = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$")
     private String ip;
+
+    @PastOrPresent // Дата не может быть из будущего
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
--- stat/stat-dto/src/main/java/ewm/dto/ViewStatsDto.java
+++ stat/stat-dto/src/main/java/ewm/dto/ViewStatsDto.java
@@ -1,10 +1,9 @@
 package ewm.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViewStatsDto {
