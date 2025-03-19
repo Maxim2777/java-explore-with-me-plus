3c3cfeaee7ee381b3048bbd09821e7c2fe6d8017
2777maxim <2777maxim@mail.ru>
2025-03-19 15:03:51 +0300

    Создана основная структура

---
+++ stat/stat-dto/src/main/java/ewm/dto/EndpointHitDto.java
@@ -0,0 +1,17 @@
+package ewm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+    private LocalDateTime timestamp;
+}
